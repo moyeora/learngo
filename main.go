9f98e26468cff6114defbbf7665f9b1745cd551e
yh22953905 <yh22953905@gmail.com>
2024-03-05 01:11:43 +0900

    1.2 Variables and Constants

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"boo/something"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+	something.SayHello()
 }
---
+++ something/something.go
@@ -0,0 +1,11 @@
+package something
+
+import "fmt"
+
+func sayBye() {
+	fmt.Println("say bye")
+}
+
+func SayHello() {
+	fmt.Println("say hello")
+}
